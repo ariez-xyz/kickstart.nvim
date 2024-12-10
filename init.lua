7ca29d312f8dcdfb0c0bc98dce4a60343ab2231d
ariez <dpape39@icloud.com>
2024-12-10 21:34:32 +0100

    telescope keybind for zk

--- init.lua
+++ init.lua
@@ -439,6 +439,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>zk', function()
+        require('telescope.builtin').find_files { cwd = '/mnt/d/Seafile/Zettelkasten' }
+      end, { desc = '[Z]ettel [K]asten' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
